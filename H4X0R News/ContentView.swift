20f99b66e1a650b9643ee4d6d1b88f60df1134c5
Steven Vandegrift <svandegrift@icloud.com>
2020-08-02 13:42:21 -0500

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Steven Vandegrift on 8/2/20.
+//  Copyright © 2020 Steven Vandegrift. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
